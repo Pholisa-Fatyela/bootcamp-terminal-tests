2d2d69df631e97cbbd20b54b5cac1d3b9fefc9ce
Pholisa-Fatyela <pholisa@projectcodex.co>
2021-07-23 15:49:26 +0200

    prompt github actions

--- test/countAllFromTown.test.js
+++ test/countAllFromTown.test.js
@@ -4,11 +4,11 @@ let assert = require("assert");
 let countAllFromTown = require("../countAllFromTown");
 
 describe('The countAllFromTown function',function(){
-  it('should take in a string of registration numbers and return how many are from a certain town',function(){
+  it('should take in a string of registration numbers and return how many are from a certain town CJ',function(){
 
     assert.strictEqual(countAllFromTown('CJ 123456, CA 123456, CJ 123456, CA 12353456, CJ 525865, CJ 345586, CA 123456, CA 12456',"CJ"),4);
   });
-  it('should take in a string of registration numbers and return how many are from a certain town',function(){
+  it('should take in a string of registration numbers and return how many are from a certain town CY',function(){
 
     assert.strictEqual(countAllFromTown('CJ 123456, CA 123456, CJ 123456, CA 12353456, CJ 525865, CJ 345586, CA 123456, CA 12456',"CY"),0);
   });
